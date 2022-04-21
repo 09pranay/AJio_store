6c80c9c8b6029fba1d2b7dfd6572a4f17c4137d8
09pranay <pranaydofe100@gmail.com>
2022-04-21 11:48:15 +0530

    delete-update, get-User , order backend, paymentScreen

--- backend/routes/orderRoytes.js
+++ backend/routes/orderRoytes.js
@@ -1,13 +1,13 @@
 import express from 'express';
 const router = express.Router()
 
-import {addOrderItems,getOrederById,updateOrderToPaid,updateOrderToDelivered,getMyOrders} from '../controllers/orderController.js'
-import {protect} from '../middleware/authMiddleware.js'
+import {addOrderItems,getOrederById,updateOrderToPaid,updateOrderToDelivered,getMyOrders,getOrders} from '../controllers/orderController.js'
+import {protect,admin} from '../middleware/authMiddleware.js'
 
-router.route('/').post(protect,addOrderItems)
+router.route('/').post(protect,addOrderItems).get(protect,admin,getOrders)
 router.route('/:id').get(protect,getOrederById)
 router.route('/:id/pay').put(protect,updateOrderToPaid)
-router.route('/:id/deliver').put(protect,updateOrderToDelivered)
-router.route('/myorders').put(protect,getMyOrders)
+router.route('/:id/deliver').put(protect,admin,updateOrderToDelivered)
+router.route('/myorders').get(protect,getMyOrders)
 
 export default router
